332ddb705badbec37f029b4254cadcada768b003
Andres0126 <acalderongu@unal.edu.co>
2025-05-02 00:13:41 -0500

    actualizar

---
+++ Introducción a python. #1.py
@@ -0,0 +1,16 @@
+print(3.0)
+print(type(3.0))
+print(type(3))
+a = 3
+print(type(a))
+#%% #crear una celda 
+
+#Se crea un objeto entero con el valor de 3.14 en memoria. La variables pi apunta a esa 
+
+pi = 3.14
+print(type(pi))
+radio = 2 
+area = pi*radio*2
+print("El area del circulo es: ", area)
+
+print(id(pi))
